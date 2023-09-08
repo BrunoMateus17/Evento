fbd91fca3f4a9d73c1e0f7f41ee09cadbfb2a2ef
Bruno Mateus <brunomateus2017@hotmail.com>
2023-09-08 15:52:25 -0300

    Add o cadastro de imagens e add listagem do mesmo

--- storage/framework/views/c4bc818ea9cfc544fbcc9d810cc95306c13d17ec.php
+++ storage/framework/views/c4bc818ea9cfc544fbcc9d810cc95306c13d17ec.php
@@ -15,7 +15,7 @@
             <?php $__currentLoopData = $events; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $event): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <div class="col-md-3">
                 <div class="card">
-                    <img src="/img/event_placeholder.jpg" alt="<?php echo e($event->title); ?>">
+                    <img src="/img/events/<?php echo e($event->image); ?>" alt="<?php echo e($event->title); ?>">
                     <div class="card-body">
                         <p class="card-date">10/09/2020</p>
                         <h5 class="card-title"><?php echo e($event->title); ?></h5>
